e5cd750c49ada7ad4407d9a2352407d2bf1e0dc5
dgatto <dgattoiii@gmail.com>
2021-07-13 18:13:46 -0400

    Stylistic.

--- src/users/users.test.ts
+++ src/users/users.test.ts
@@ -20,56 +20,38 @@ describe('UsersService', function () {
     expect(usersService).to.be.ok;
   });
 
-  describe('getUserById', () => {
-    it('should return error and if no user is found', async () => {
-      findByUserIdMock = sinon
-        .stub(statics, 'findUserById')
-        .callsFake(async () => await null);
+  it('should return error and if no user is found', async () => {
+    findByUserIdMock = sinon
+      .stub(statics, 'findUserById')
+      .callsFake(async () => await null);
 
-      const userId = '6088b864a6c9b94094b43e42';
-      const response = await usersService.getUserById(userId);
+    const userId = '6088b864a6c9b94094b43e42';
+    const response = await usersService.getUserById(userId);
 
-      expect(response).to.deep.equal({
-        status: false,
-        message: 'User not found.',
-      });
-    });
-
-    it('should return user if a user is found', async () => {
-      findByUserIdMock = sinon
-        .stub(statics, 'findUserById')
-        .callsFake(async () => await user);
-
-      const _id = '6088b864a6c9b94094b43e42';
-      const user = new User({
-        _id,
-        username: 'test-automation',
-        associatedProtests: [],
-      });
-
-      const response = await usersService.getUserById(_id);
-
-      expect(response).to.deep.equal({
-        status: true,
-        message: 'User successfully found.',
-        payload: { user },
-      });
+    expect(response).to.deep.equal({
+      status: false,
+      message: 'User not found.',
     });
   });
 
-  // describe('getByUsername', () => {
-  //   it('should return error if user is not found', () => {});
+  it('should return user if a user is found', async () => {
+    findByUserIdMock = sinon
+      .stub(statics, 'findUserById')
+      .callsFake(async () => await user);
 
-  //   it('should return user if user is found', () => {});
-  // });
+    const _id = '6088b864a6c9b94094b43e42';
+    const user = new User({
+      _id,
+      username: 'test-automation',
+      associatedProtests: [],
+    });
 
-  // describe('authenticateUser', () => {
-  //   it('should return error if secret key not set', () => {});
+    const response = await usersService.getUserById(_id);
 
-  //   it('should return error if user is not found', () => {});
-
-  //   it('should return error if passwords do not match', () => {});
-  // });
-
-  // describe('registerUser', () => {});
+    expect(response).to.deep.equal({
+      status: true,
+      message: 'User successfully found.',
+      payload: { user },
+    });
+  });
 });
