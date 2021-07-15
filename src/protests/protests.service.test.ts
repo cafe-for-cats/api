2ae77e3381ec335465bb4929dc14b5e41bb20ba0
dgatto <dgattoiii@gmail.com>
2021-07-14 22:13:16 -0400

    Cleanup; Fix tests;

--- src/protests/protests.service.test.ts
+++ src/protests/protests.service.test.ts
@@ -1,18 +1,11 @@
 import { expect } from 'chai';
 import sinon, { SinonStubbedInstance } from 'sinon';
 import { ProtestsService } from './protests.service';
-import * as protestStatics from './protests.statics';
-import * as userStatics from '../users/users.statics';
 import { UsersService } from '../users/users.service';
-import User from '../users/users.models';
-import Protest from './protests.models';
-import { ObjectId } from 'mongodb';
 
 describe('ProtestsService', function () {
   let protestsService: ProtestsService;
   let usersService: UsersService;
-  let findByUserIdMock: SinonStubbedInstance<typeof userStatics.findUserById>;
-  let addProtestMock: SinonStubbedInstance<typeof protestStatics.addProtest>;
 
   beforeEach(() => {
     protestsService = new ProtestsService();
