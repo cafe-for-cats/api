fe6de183586aca798d70298aea602d4fed0a9f48
dgatto <dgattoiii@gmail.com>
2021-07-17 20:27:42 -0400

    foo

--- src/protests/protests.service.test.ts
+++ src/protests/protests.service.test.ts
@@ -4,9 +4,6 @@ import { ProtestsService } from './protests.service';
 import * as protestStatics from './protests.statics';
 import * as userStatics from '../users/users.statics';
 import { UsersService } from '../users/users.service';
-import User from '../users/users.models';
-import Protest from './protests.models';
-import { ObjectId } from 'mongodb';
 
 describe('ProtestsService', function () {
   let protestsService: ProtestsService;
